74770d5736949db49229d56e3c38c52563665d36
yeeun <dpdms9621@naver.com>
2022-05-27 22:46:07 +0900

    헤더css 알림 추가

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,16 +1,26 @@
-import React from "react";
+import React, {useState, useEffect, useRef} from "react";
 import "../css/Header.css";
 import "../Main.css";
 import logo from "../image/muleoba_logo.png";
 import { FaBell, FaBars, FaSearch } from "react-icons/fa";
 
-const Header = () => {
+
+
+
+export default function Header(){
+  const [alarm, setAalarm] = useState(false);
+
+  function onClickalarm(){
+    setAalarm(true);
+  }
+
   return (
     <div className="header">
-      <div className="container header_flex">
+      <div className="container ">
+        <div className="header_flex">
         <div className="header_left">
-          <div className="header_logo">
-            <img src={logo} />
+          <div className="header_logo_box">
+            <img src={logo} className="header_logo"/>
           </div>
           <div className="header_searchBar">
             <input type="text" placeholder="물품 검색" />
@@ -23,15 +33,21 @@ const Header = () => {
             <div className="header_rankingNickname">의왕시피바다</div>
           </div>
           <div className="header_bell">
-            <FaBell className="header_bellIcon" />
+            <FaBell className="header_bellIcon" onClick={() => setAalarm(!alarm)}/>
           </div>
           <div className="header_menuBar">
             <FaBars className="header_barIcon" />
           </div>
         </div>
+        </div>
+        <div className="header_alarm_bar">
+          {
+              alarm ?
+              <div className="header_alarm_box">알람</div>
+              :null
+            }
+        </div>    
       </div>
     </div>
   );
 };
-
-export default Header;
