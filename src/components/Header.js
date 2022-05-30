08c2fff0bb5622ee0a5e9e0aea49f90d8010936f
yeeun <dpdms9621@naver.com>
2022-05-30 16:20:53 +0900

    5.30 중간

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,7 +2,7 @@ import React, {useState, useEffect, useRef} from "react";
 import "../css/Header.css";
 import "../Main.css";
 import logo from "../image/muleoba_logo.png";
-import { FaBell, FaBars, FaSearch } from "react-icons/fa";
+import { FaBell, FaBars, FaSearch,FaTrophy } from "react-icons/fa";
 
 
 
@@ -29,8 +29,13 @@ export default function Header(){
         </div>
         <div className="header_right">
           <div className="header_ranking">
+            <div className="header_rankingIcon">
+             <FaTrophy className="header_ranking_icon" />
+            </div>
+            <div className="header_ranking_box">
             <div className="header_rankingNumber">3</div>
             <div className="header_rankingNickname">의왕시피바다</div>
+            </div>
           </div>
           <div className="header_bell">
             <FaBell className="header_bellIcon" onClick={() => setAalarm(!alarm)}/>
